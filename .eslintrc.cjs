9a8f086ba00f828472897c6616c82073ba914611
javalce <javalce29@gmail.com>
2024-07-18 15:40:09 +0200

    feat: add "Añadir documento" page to admin dashboard

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -37,6 +37,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-floating-promises': 'off',
     '@typescript-eslint/no-confusing-void-expression': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
     // React rules
     'react/jsx-sort-props': [
       'warn',
--- src/app/admin/dashboard/add-document/page.tsx
+++ src/app/admin/dashboard/add-document/page.tsx
@@ -1,7 +1,10 @@
+import { AddDocumentForm } from '@/components/dashboard/add-document-form';
+
 export default function Page() {
   return (
     <div className='flex flex-col gap-y-6'>
       <h3 className='text-2xl'>Añadir documento</h3>
+      <AddDocumentForm />
     </div>
   );
 }
