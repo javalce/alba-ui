2d693f49d2dc83715e1cc1db60a4b4cca29e269d
javalce <javalce29@gmail.com>
2024-07-18 11:17:47 +0200

    build: update dependencies and fix code formatting

--- src/components/chat/chat-message.tsx
+++ src/components/chat/chat-message.tsx
@@ -19,7 +19,7 @@ export function ChatMessage({ message }: { message: Message }) {
       </div>
       <div className='ml-4 flex-1 space-y-2 overflow-hidden px-1'>
         <MemoizedReactMarkdown
-          className='prose break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0'
+          className='prose dark:prose-invert prose-p:leading-relaxed prose-pre:p-0 break-words'
           components={{
             /* eslint-disable react/no-unstable-nested-components -- required for remark plugins */
             p({ children }) {
--- src/providers/theme.tsx
+++ src/providers/theme.tsx
@@ -1,7 +1,7 @@
-'use client'
+'use client';
 
 import { ThemeProvider as NextThemeProvider } from 'next-themes';
-import { ThemeProviderProps } from 'next-themes/dist/types';
+import { type ThemeProviderProps } from 'next-themes/dist/types';
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
