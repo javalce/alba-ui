c7dddd72e931192033f75d3d61755ad1099fe81b
javalce <javalce29@gmail.com>
2024-07-26 14:11:16 +0200

    refactor: update header component with admin dashboard link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,15 @@
+import { MenuLink } from '@/components/menu-link';
+import Link from 'next/link';
+
 export function Header() {
   return (
     <header className='sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between border-b bg-background/80 px-4'>
-      <h1 className='scroll-m-20 text-3xl font-extrabold tracking-tight'>Alba</h1>
+      <Link href='/' className='scroll-m-20 text-4xl font-bold tracking-tight'>
+        Alba
+      </Link>
+      <section className='flex items-center justify-center gap-x-3'>
+        <MenuLink href='/admin/dashboard'>Dashboard</MenuLink>
+      </section>
     </header>
   );
 }
