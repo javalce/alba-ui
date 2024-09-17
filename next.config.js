a8507fe7f33a6cf0a67883fd20894007bb4400be
javalce <javalce29@gmail.com>
2024-09-17 15:24:42 +0200

    feat: set basePath to '/alba' in Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 // @ts-check
 
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  basePath: isProd ? '/alba' : '',
 };
 
 module.exports = nextConfig;
