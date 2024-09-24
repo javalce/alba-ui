e742a70dd645f168aeb2d4628a70dfa7b5b26b28
javalce <javalce29@gmail.com>
2024-09-24 12:44:06 +0200

    feat: update auth basePath for production environment

--- src/auth.ts
+++ src/auth.ts
@@ -102,4 +102,5 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
     signIn: '/login',
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/alba/api/auth' : '/api/auth',
 });
