9600fd36a9b7f5b638426c581c7d1be76243a040
javalce <javalce29@gmail.com>
2024-09-25 11:19:09 +0200

    feat: update basePath configuration to use APP_BASE_PATH environment variable

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 // @ts-check
 
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  basePath: isProd ? '/alba' : '',
+  basePath: process.env.APP_BASE_PATH,
 };
 
 module.exports = nextConfig;
