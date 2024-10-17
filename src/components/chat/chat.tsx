ca8b9f02d16b9327ee9839b7799dd00285b0ffb0
javalce <javalce29@gmail.com>
2024-10-17 11:50:48 +0200

    feat: add reset chat functionality with unique ID generation

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -1,8 +1,12 @@
 'use client';
 
+import { Pencil2Icon } from '@radix-ui/react-icons';
+import { generateId } from 'ai';
 import { useChat } from 'ai/react';
+import { useState } from 'react';
 import { toast } from 'sonner';
 
+import { Button } from '@/components/ui/button';
 import { API_URL } from '@/constants';
 import { useScrollToBottom } from '@/hooks/use-scroll-to-bottom';
 
@@ -11,7 +15,9 @@ import { ChatList } from './chat-list';
 import { EmptyScreen } from './empty-screen';
 
 export function Chat() {
+  const [id, setId] = useState(() => generateId());
   const { messages, input, handleInputChange, handleSubmit, isLoading } = useChat({
+    id,
     api: `${API_URL}/chat`,
     streamProtocol: 'text',
     onError: (err) => {
@@ -21,8 +27,17 @@ export function Chat() {
 
   const { containerRef, endRef } = useScrollToBottom<HTMLDivElement>();
 
+  function resetChat() {
+    setId(generateId());
+  }
+
   return (
     <div className='flex h-[calc(100dvh_-theme(spacing.16))] flex-row justify-center pb-4 md:pb-8'>
+      <div className='fixed left-0 ml-4 mt-4'>
+        <Button variant='secondary' onClick={resetChat}>
+          <Pencil2Icon className='mr-2 size-5' /> Nuevo chat
+        </Button>
+      </div>
       <div className='mt-10 flex flex-col items-center justify-between gap-4'>
         <div
           ref={containerRef}
